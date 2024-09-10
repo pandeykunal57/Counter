84d11df1d7db890e1b8d6e9eeb8405cd294e96ff
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2024-09-10 13:11:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function increment()
+{
+ var number= Number(document.getElementById('number').innerHTML)
+
+ document.getElementById('number').innerHTML=number+1
+}
+
+function decrement()
+{
+    var number= Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML=number-1  
+}
+
+function nuetral()
+{
+    document.getElementById('number').innerHTML=0
+}
+
+
+
+
+
+
+
+
+
+
